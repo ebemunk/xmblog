f932bb6d7d5d6bdad458ac84e4cf39c213bce108
Bugra Firat <bfirat@xmatters.com>
2019-03-05 16:27:50 -0800

    part4

--- frontend-testing-paradigms/part4/src/Todos.test.js
+++ frontend-testing-paradigms/part4/src/Todos.test.js
@@ -11,13 +11,10 @@ const items = [
 ];
 
 test("renders the items", () => {
-  const { container } = render(<Todos list={items} />);
+  const { container, getByText } = render(<Todos list={items} />);
 
   expect(container.querySelectorAll(".item").length).toBe(4);
   expect(container.querySelectorAll(".complete").length).toBe(2);
-
-  // We need to add these tests below to catch the boolean logic error
-  // that is made inside Item.jsx
 
   expect(container.querySelectorAll(".item")[1].className).toBe(
     "item complete"
@@ -25,6 +22,13 @@ test("renders the items", () => {
   expect(container.querySelectorAll(".item")[2].className).toBe(
     "item complete"
   );
+
+  // We need to add even more tests now to avoid this kind of error.
+  // Notice how the Snapshot test diff is much more descriptive
+  // and less tedious to write and maintain
+  items.forEach(item => {
+    expect(getByText(item.name)).toBeDefined();
+  });
 });
 
 test("renders the items - SNAPSHOT", () => {
